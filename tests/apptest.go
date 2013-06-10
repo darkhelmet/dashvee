fa13d19433eaebef2797aa9558654eb371aa23ac
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2013-06-09 23:48:43 -0600

    moar tests

--- tests/apptest.go
+++ tests/apptest.go
@@ -1,21 +1,39 @@
 package tests
 
-import "github.com/robfig/revel"
+import (
+    "github.com/darkhelmet/dashvee/app/controllers"
+    "github.com/robfig/revel"
+)
 
 type AppTest struct {
-	revel.TestSuite
+    revel.TestSuite
 }
 
-func (t AppTest) Before() {
-	println("Set up")
+func (t AppTest) TestPostsLoad() {
+    controllers.NewRepo("posts")
 }
 
-func (t AppTest) TestThatIndexPageWorks() {
-	t.Get("/")
-	t.AssertOk()
-	t.AssertContentType("text/html")
+func (t AppTest) TestPagesLoad() {
+    controllers.NewRepo("pages")
 }
 
-func (t AppTest) After() {
-	println("Tear down")
+func (t AppTest) TestTimeZones() {
+    repo := controllers.NewRepo("posts")
+    posts, _ := repo.All()
+    for _, post := range posts {
+        name, offset := post.PublishedOn.Zone()
+        t.Assertf(offset != 0, "got offset 0 in zone %s for %s, expected not 0", name, post.Title)
+    }
+}
+
+func (t AppTest) TestIndex() {
+    t.Get("/")
+    t.AssertOk()
+    t.AssertContentType("text/html")
+}
+
+func (t AppTest) TestOpenSearch() {
+    t.Get("/opensearch.xml")
+    t.AssertOk()
+    t.AssertContentType("application/xml; charset=utf-8")
 }
