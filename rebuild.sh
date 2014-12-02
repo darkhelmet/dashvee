362f8a990447196f9688939ccfde56384578a3f5
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2014-12-02 00:58:40 -0700

    reverse tagging

--- rebuild.sh
+++ rebuild.sh
@@ -3,6 +3,6 @@ set -e
 set -x
 
 tag=$1
-docker build -t dashvee:$tag .
-docker tag dashvee:$tag dashvee:latest
+docker build -t dashvee:latest .
+docker tag dashvee:latest dashvee:$tag
 docker restart $(docker ps | grep dashvee | cut -f1 -d' ')
