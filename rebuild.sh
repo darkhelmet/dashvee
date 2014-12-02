34b961617efe7505df4f1ab4e4f4a92e993b306a
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2014-12-02 00:53:58 -0700

    this should work now

--- rebuild.sh
+++ rebuild.sh
@@ -5,4 +5,4 @@ set -x
 tag=$1
 docker build -t dashvee:$tag .
 docker tag dashvee:$tag dashvee:latest
-# docker restart $(docker ps | grep dashvee | cut -f1 -d' ')
+docker restart $(docker ps | grep dashvee | cut -f1 -d' ')
