fd524835018746b657a3931ed996c58fe70eb9b4
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2015-02-17 00:54:20 -0700

    fix build script

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ git push
 ruby -r erb -e "print ERB.new(File.read('Dockerfile.erb')).result" > Dockerfile
 sha=$(git rev-parse --short HEAD)
 docker build -t darkhelmetlive/dashvee:latest .
-docker tag darkhelmetlive/dashvee:latest darkhelmetlive/dashvee:${sha}
+docker tag -f darkhelmetlive/dashvee:latest darkhelmetlive/dashvee:${sha}
 docker push darkhelmetlive/dashvee:latest
-docker push darkhelmetlive/dashvee::${sha}
+docker push darkhelmetlive/dashvee:${sha}
