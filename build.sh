872c14542c47df6e29863130c918f684ad005e45
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2015-02-17 00:44:21 -0700

    ansible for deployment

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+git push
+ruby -r erb -e "print ERB.new(File.read('Dockerfile.erb')).result" > Dockerfile
+sha=$(git rev-parse --short HEAD)
+docker build -t darkhelmetlive/dashvee:latest .
+docker tag darkhelmetlive/dashvee:latest darkhelmetlive/dashvee:${sha}
+docker push darkhelmetlive/dashvee:latest
+docker push darkhelmetlive/dashvee::${sha}
--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-set -x
-
-git push
-ruby -r erb -e "print ERB.new(File.read('Dockerfile.erb')).result" > Dockerfile
-scp -q Dockerfile dashvee:Dockerfile
-scp -q rebuild.sh dashvee:rebuild.sh
-ssh dashvee "./rebuild.sh $(git rev-parse --short HEAD)"
