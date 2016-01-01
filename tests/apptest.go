051542094044c585cea75b8ea74b0eae20ace211
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2016-01-01 16:30:51 -0400

    fix tests

--- tests/apptest.go
+++ tests/apptest.go
@@ -2,11 +2,11 @@ package tests
 
 import (
 	"github.com/darkhelmet/dashvee/app/controllers"
-	"github.com/revel/revel"
+	"github.com/revel/revel/testing"
 )
 
 type AppTest struct {
-	revel.TestSuite
+	testing.TestSuite
 }
 
 func (t AppTest) TestPostsLoad() {
