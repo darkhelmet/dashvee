63ec532fb6172e339956ac8fdd9745e18a85768c
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2016-03-28 17:34:37 -0300

    try printing canonical host

--- Godeps/_workspace/src/github.com/darkhelmet/webutil/canonical_host.go
+++ Godeps/_workspace/src/github.com/darkhelmet/webutil/canonical_host.go
@@ -1,26 +1,28 @@
 package webutil
 
 import (
-    "net/http"
-    "net/url"
+	"log"
+	"net/http"
+	"net/url"
 )
 
 type CanonicalHostHandler struct {
-    H                     http.Handler
-    CanonicalHost, Scheme string
+	H                     http.Handler
+	CanonicalHost, Scheme string
 }
 
 func (chh CanonicalHostHandler) replaceHost(u url.URL) string {
-    u.Host = chh.CanonicalHost
-    u.Scheme = chh.Scheme
-    return u.String()
+	u.Host = chh.CanonicalHost
+	u.Scheme = chh.Scheme
+	return u.String()
 }
 
 func (chh CanonicalHostHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    if r.Host != chh.CanonicalHost {
-        u := chh.replaceHost(*r.URL)
-        http.Redirect(w, r, u, http.StatusMovedPermanently)
-        return
-    }
-    chh.H.ServeHTTP(w, r)
+	log.Printf("canonical host: %s", r.Host)
+	if r.Host != chh.CanonicalHost {
+		u := chh.replaceHost(*r.URL)
+		http.Redirect(w, r, u, http.StatusMovedPermanently)
+		return
+	}
+	chh.H.ServeHTTP(w, r)
 }
