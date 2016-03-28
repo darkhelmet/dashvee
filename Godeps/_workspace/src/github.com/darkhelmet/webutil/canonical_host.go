d69bc4d1ce3a2b39b8b3d4fa9059d200745235f7
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2016-03-28 17:55:08 -0300

    Revert "try printing canonical host"
    This reverts commit 63ec532fb6172e339956ac8fdd9745e18a85768c.

--- Godeps/_workspace/src/github.com/darkhelmet/webutil/canonical_host.go
+++ Godeps/_workspace/src/github.com/darkhelmet/webutil/canonical_host.go
@@ -1,28 +1,26 @@
 package webutil
 
 import (
-	"log"
-	"net/http"
-	"net/url"
+    "net/http"
+    "net/url"
 )
 
 type CanonicalHostHandler struct {
-	H                     http.Handler
-	CanonicalHost, Scheme string
+    H                     http.Handler
+    CanonicalHost, Scheme string
 }
 
 func (chh CanonicalHostHandler) replaceHost(u url.URL) string {
-	u.Host = chh.CanonicalHost
-	u.Scheme = chh.Scheme
-	return u.String()
+    u.Host = chh.CanonicalHost
+    u.Scheme = chh.Scheme
+    return u.String()
 }
 
 func (chh CanonicalHostHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Printf("canonical host: %s", r.Host)
-	if r.Host != chh.CanonicalHost {
-		u := chh.replaceHost(*r.URL)
-		http.Redirect(w, r, u, http.StatusMovedPermanently)
-		return
-	}
-	chh.H.ServeHTTP(w, r)
+    if r.Host != chh.CanonicalHost {
+        u := chh.replaceHost(*r.URL)
+        http.Redirect(w, r, u, http.StatusMovedPermanently)
+        return
+    }
+    chh.H.ServeHTTP(w, r)
 }
